86425b653f854bdaeb0fc0cf783b6eb3121c2828
Malte Petersen <mp@marketoolz.com>
2023-05-22 23:21:18 +0200

    Test gefixt

--- apps/frontend/src/app/app.component.spec.ts
+++ apps/frontend/src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   let component: AppComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, RouterTestingModule],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   });
 
