05944aa0c22de7eb50258a99d8226b904f0cfb82
Malte Petersen <mp@marketoolz.com>
2023-05-22 23:07:37 +0200

    Eslint eingefügt

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*.{json,css,scss,md,html}': ['prettier --write'],
-  '*.{js,ts}': ['tslint --fix', 'prettier --write'],
+  '*.{js,ts}': ['eslint --fix', 'prettier --write'],
 };
