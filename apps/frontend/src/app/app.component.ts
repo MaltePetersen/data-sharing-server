7d0d3fd66955b88e91b5f56c59a1efa90a1ac61d
Malte Petersen <mp@marketoolz.com>
2023-06-22 20:19:30 +0200

    Daten werden persistiert

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { DataService } from './services/data.service';
+import { FileService } from './services/files.service';
+import { state } from '@angular/animations';
+import { Token } from '@angular/compiler';
+import { Route, Router } from '@angular/router';
+import { webSocket } from "rxjs/webSocket";
 
 @Component({
   selector: 'data-sharing-server-root',
@@ -6,4 +12,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private dataService: DataService, private fileService: FileService, private router: Router){
+ const state = dataService.getState()
+    if(state && this.fileStillExists(state.token.creation)){
+      fileService.updateFileName(state.fileName)
+      fileService.updateToken(state.token)
+      this.router.navigate(['/dashboard'])
+    }
+  }
+  private fileStillExists(creation: Date){
+    const deepCopy = new Date(JSON.parse(JSON.stringify(creation)));
+    deepCopy.setMinutes(deepCopy.getMinutes() + 30);
+    return (deepCopy.getTime() - new Date(Date.now()).getTime()) > 0
+  }
 }
